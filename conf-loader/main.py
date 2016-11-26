fc026549c879305ab8c6bf8979a7c7842dc23a88
Abriko <abriko@live.ie>
2016-11-26 16:07:53 +0800

    add gitlab files support

--- conf-loader/main.py
+++ conf-loader/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
 
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -15,11 +14,22 @@ import os
 import sys
 import logging
 import yaml
+import requests
 import jinja2
 
 from jinja2.exceptions import UndefinedError
 
 standard_library.install_aliases()
+
+# Enable verified HTTPS requests on older Pythons
+# http://urllib3.readthedocs.org/en/latest/security.html
+if sys.version_info[0] == 2:
+    requests.packages.urllib3.contrib.pyopenssl.inject_into_urllib3()
+    from urllib import quote
+    from urllib import urlencode
+else:
+    from urllib.parse import quote
+    from urllib.parse import urlencode
 
 BASE_DIR = os.getcwd()
 
@@ -29,6 +39,7 @@ with open("%s/.ci/config/config.yml" % BASE_DIR, 'r') as f:
     except yaml.YAMLError as exc:
         print(exc)
         sys.exit(1)
+
 if os.environ.get('confloader_debug'):
     if os.environ['confloader_debug']:
         debug_level = logging.DEBUG
@@ -40,6 +51,13 @@ else:
     else:
         debug_level = logging.INFO
 
+if DATA.get('gitlab_token'):
+    GITLAB_HEADERS = {'PRIVATE-TOKEN': DATA['gitlab_token']}
+    GITLAB_BASEURL = DATA['gitlab_baseurl']
+else:
+    GITLAB_HEADERS = None
+
+REQUESET_TIMEOUT = 20
 
 logging.basicConfig(level=debug_level,
                     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -49,7 +67,7 @@ logging.basicConfig(level=debug_level,
 
 
 def _create_conf(tenv, config):
-    # Use template file to create config
+    # Use system environ and template file to create config
     logging.debug("Loading template %s", config['src'])
     template = tenv.get_template(config['src'])
 
@@ -61,7 +79,28 @@ def _create_conf(tenv, config):
                           config['src'], err)
             sys.exit(1)
 
-    logging.info("%s has been created successfully.", config['dest'])
+    logging.info("%s from template has been created successfully.",
+                 config['dest'])
+
+
+def _gitlab_conf(config):
+    # Download gitlab files with token
+    # https://gitlab.com/<group_name>/<project_name>/raw/master/<f??older>/<file_name>?p??rivate_token=<your_key>
+    logging.debug("Loading gitlab files %s", config['path'])
+    with open(config['dest'], 'wb') as handle:
+        url = "%s/%s" % (GITLAB_BASEURL, config['path'])
+        response = requests.get(url, headers=GITLAB_HEADERS,
+                                timeout=REQUESET_TIMEOUT, stream=True,
+                                allow_redirects=False)
+
+        if response.status_code != 200:
+            raise RuntimeError("Cant load gitlab files", response.text)
+
+        for block in response.iter_content(1024):
+            handle.write(block)
+
+    logging.info("%s from gitlab has been created successfully.",
+                 config['dest'])
 
 
 def main():
@@ -76,6 +115,8 @@ def main():
     for config in DATA['configs']:
         if config['type'] == 'tpl':
             _create_conf(templateEnv, config)
+        elif config['type'] == 'gitlab':
+            _gitlab_conf(config)
 
     sys.exit(0)
 
